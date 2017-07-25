f892e34bba33a32dc293fe1aadfe094dc0efb7fe
CromixPT <30273963+CromixPT@users.noreply.github.com>
2017-07-25 02:47:54 +0100

    S2C22

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,30 +1,38 @@
 #include <iostream>
 #include <string>
 
-int main() {
+void PrintIntro();
+std::string GetGuess();
 
-	//Introduce the game
+int main() 
+{
+	PrintIntro();
+	constexpr int MAX_TRIES = 5;
+	for (int count = 1; count <= MAX_TRIES; ++count)
+	{
+		std::cout << "Your guess was: " << GetGuess() << std::endl;
+		std::cout << std::endl;
+	}
+	return 0;
+}
+
+
+//Get guess from user
+std::string GetGuess()
+{
+	std::string Guess = "";
+	std::cout << "Input your guess: ";
+	std::getline(std::cin, Guess);
+	std::cin.sync();
+	return Guess;
+}
+
+//Introduce the game
+void PrintIntro()
+{
 	constexpr int WORD_LEGHT = 5;
 	std::cout << "Welcome to Bulls & Cows, a fun word game\n";
 	std::cout << "Can you guess the " << WORD_LEGHT;
-	std::cout << " letter isogram I'm thinking of?\n";
-
-	std::string Guess = "";
-
-	// get a guess form user
-	std::cout << "Input your guess: ";
-	std::getline(std::cin, Guess);
-	//repeat the guess back to user
-	std::cout << "Your guess was: " << Guess << std::endl;
-	std::cin.sync();
-
-	// get a guess form user
-	std::cout << "Input your guess: ";
-	std::getline(std::cin, Guess);
-	//repeat the guess back to user
-	std::cout << "Your guess was: " << Guess << std::endl;
-
-
-
-	return 0;
+	std::cout << " letter isogram I'm thinking of?\n\n";
+	return;
 }
