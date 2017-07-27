a5a2dc63ce41ddf0fa7935c1574a79df95657b65
CromixPT <30273963+CromixPT@users.noreply.github.com>
2017-07-27 02:01:46 +0100

    S02C28

---
+++ BullCowGame/FBullCowGame.cpp
@@ -0,0 +1,29 @@
+#include "FBullCowGame.h"
+
+void FBullCowGame::Reset()
+{
+	return;
+}
+
+
+int FBullCowGame::GetMaxTries()
+{
+	return 0;
+}
+
+int FBullCowGame::GetCurrentTry()
+{
+	return 0;
+}
+
+
+bool FBullCowGame::CheckGuessValidity(std::string)
+{
+	return false;
+}
+
+
+bool FBullCowGame::IsGameWon()
+{
+	return false;
+}
